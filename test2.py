31c405d1653b8443e7ac8a30b774f1de90ae8482
Peter Slijkhuis <p.slijkhuis@gmail.com>
2020-01-13 14:20:23 +0100

    Test DAG

--- test2.py
+++ test2.py
@@ -1,53 +1,91 @@
-from airflow import DAG
-from datetime import datetime, timedelta
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.operators.dummy_operator import DummyOperator
+from airflow.contrib.kubernetes.secret import Secret
+from airflow.contrib.kubernetes.volume import Volume
+from airflow.contrib.kubernetes.volume_mount import VolumeMount
+from airflow.contrib.kubernetes.pod import Port
 
 
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "start_date": datetime.utcnow(),
-    "email": ["airflow@example.com"],
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
-    "concurrency": 2,
+secret_file = Secret('volume', '/etc/sql_conn', 'airflow-secrets', 'sql_alchemy_conn')
+secret_env  = Secret('env', 'SQL_CONN', 'airflow-secrets', 'sql_alchemy_conn')
+secret_all_keys  = Secret('env', None, 'airflow-secrets-2')
+volume_mount = VolumeMount('test-volume',
+                            mount_path='/root/mount_file',
+                            sub_path=None,
+                            read_only=True)
+port = Port('http', 80)
+configmaps = ['test-configmap-1', 'test-configmap-2']
+
+
+affinity = {
+    'nodeAffinity': {
+      'preferredDuringSchedulingIgnoredDuringExecution': [
+        {
+          "weight": 1,
+          "preference": {
+            "matchExpressions": {
+              "key": "disktype",
+              "operator": "In",
+              "values": ["ssd"]
+            }
+          }
+        }
+      ]
+    },
+    "podAffinity": {
+      "requiredDuringSchedulingIgnoredDuringExecution": [
+        {
+          "labelSelector": {
+            "matchExpressions": [
+              {
+                "key": "security",
+                "operator": "In",
+                "values": ["S1"]
+              }
+            ]
+          },
+          "topologyKey": "failure-domain.beta.kubernetes.io/zone"
+        }
+      ]
+    },
+    "podAntiAffinity": {
+      "requiredDuringSchedulingIgnoredDuringExecution": [
+        {
+          "labelSelector": {
+            "matchExpressions": [
+              {
+                "key": "security",
+                "operator": "In",
+                "values": ["S2"]
+              }
+            ]
+          },
+          "topologyKey": "kubernetes.io/hostname"
+        }
+      ]
+    }
 }
 
-dag = DAG(
-    "kubernetes_sample",
-    default_args=default_args,
-    schedule_interval=timedelta(minutes=10),
-)
+tolerations = [
+    {
+        'key': "key",
+        'operator': 'Equal',
+        'value': 'value'
+     }
+]
 
+k = KubernetesPodOperator(namespace='default',
+                          image="ubuntu:16.04",
+                          cmds=["bash", "-cx"],
+                          arguments=["echo", "10"],
+                          labels={"foo": "bar"},
+                          secrets=[secret_file, secret_env, secret_all_keys],
+                          ports=[port]
+                          name="test",
+                          task_id="task",
+                          affinity=affinity,
+                          is_delete_operator_pod=True,
+                          hostnetwork=False,
+                          tolerations=tolerations,
+                          configmaps=configmaps
+                          )
 
-start = DummyOperator(task_id="run_this_first", dag=dag)
-
-passing = KubernetesPodOperator(
-    namespace="default",
-    image="Python:3.6",
-    cmds=["Python", "-c"],
-    arguments=["print('hello world')"],
-    labels={"foo": "bar"},
-    name="passing-test",
-    task_id="passing-task",
-    get_logs=True,
-    dag=dag,
-)
-
-failing = KubernetesPodOperator(
-    namespace="default",
-    image="ubuntu:1604",
-    cmds=["Python", "-c"],
-    arguments=["print('hello world')"],
-    labels={"foo": "bar"},
-    name="fail",
-    task_id="failing-task",
-    get_logs=True,
-    dag=dag,
-)
-
-passing.set_upstream(start)
-failing.set_upstream(start)
