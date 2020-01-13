2707df3d917ebc547e79a8f07ffabce112e33ba4
Peter Slijkhuis <p.slijkhuis@gmail.com>
2020-01-13 20:41:14 +0100

    Add another DAG

--- test2.py
+++ test2.py
@@ -1,91 +1,102 @@
-from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
-from airflow.contrib.kubernetes.secret import Secret
-from airflow.contrib.kubernetes.volume import Volume
-from airflow.contrib.kubernetes.volume_mount import VolumeMount
-from airflow.contrib.kubernetes.pod import Port
+# -*- coding: utf-8 -*-
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""
+This is an example dag for using the Kubernetes Executor.
+"""
+import os
 
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
 
-secret_file = Secret('volume', '/etc/sql_conn', 'airflow-secrets', 'sql_alchemy_conn')
-secret_env  = Secret('env', 'SQL_CONN', 'airflow-secrets', 'sql_alchemy_conn')
-secret_all_keys  = Secret('env', None, 'airflow-secrets-2')
-volume_mount = VolumeMount('test-volume',
-                            mount_path='/root/mount_file',
-                            sub_path=None,
-                            read_only=True)
-port = Port('http', 80)
-configmaps = ['test-configmap-1', 'test-configmap-2']
+args = {"owner": "airflow", "start_date": days_ago(2)}
 
+with DAG(
+    dag_id="example_kubernetes_executor", default_args=args, schedule_interval=None
+) as dag:
 
-affinity = {
-    'nodeAffinity': {
-      'preferredDuringSchedulingIgnoredDuringExecution': [
-        {
-          "weight": 1,
-          "preference": {
-            "matchExpressions": {
-              "key": "disktype",
-              "operator": "In",
-              "values": ["ssd"]
-            }
-          }
-        }
-      ]
-    },
-    "podAffinity": {
-      "requiredDuringSchedulingIgnoredDuringExecution": [
-        {
-          "labelSelector": {
-            "matchExpressions": [
-              {
-                "key": "security",
-                "operator": "In",
-                "values": ["S1"]
-              }
+    affinity = {
+        "podAntiAffinity": {
+            "requiredDuringSchedulingIgnoredDuringExecution": [
+                {
+                    "topologyKey": "kubernetes.io/hostname",
+                    "labelSelector": {
+                        "matchExpressions": [
+                            {"key": "app", "operator": "In", "values": ["airflow"]}
+                        ]
+                    },
+                }
             ]
-          },
-          "topologyKey": "failure-domain.beta.kubernetes.io/zone"
         }
-      ]
-    },
-    "podAntiAffinity": {
-      "requiredDuringSchedulingIgnoredDuringExecution": [
-        {
-          "labelSelector": {
-            "matchExpressions": [
-              {
-                "key": "security",
-                "operator": "In",
-                "values": ["S2"]
-              }
-            ]
-          },
-          "topologyKey": "kubernetes.io/hostname"
-        }
-      ]
     }
-}
 
-tolerations = [
-    {
-        'key': "key",
-        'operator': 'Equal',
-        'value': 'value'
-     }
-]
+    tolerations = [{"key": "dedicated", "operator": "Equal", "value": "airflow"}]
 
-k = KubernetesPodOperator(namespace='default',
-                          image="ubuntu:16.04",
-                          cmds=["bash", "-cx"],
-                          arguments=["echo", "10"],
-                          labels={"foo": "bar"},
-                          secrets=[secret_file, secret_env, secret_all_keys],
-                          ports=[port]
-                          name="test",
-                          task_id="task",
-                          affinity=affinity,
-                          is_delete_operator_pod=True,
-                          hostnetwork=False,
-                          tolerations=tolerations,
-                          configmaps=configmaps
-                          )
+    def print_stuff():  # pylint: disable=missing-docstring
+        print("stuff!")
 
+    def use_zip_binary():
+        """
+        Checks whether Zip is installed.
+        :return: True if it is installed, False if not.
+        :rtype: bool
+        """
+        return_code = os.system("zip")
+        if return_code != 0:
+            raise SystemError("The zip binary is missing")
+
+    # You don't have to use any special KubernetesExecutor configuration if you don't want to
+    start_task = PythonOperator(task_id="start_task", python_callable=print_stuff)
+
+    # But you can if you want to
+    one_task = PythonOperator(
+        task_id="one_task",
+        python_callable=print_stuff,
+        executor_config={"KubernetesExecutor": {"image": "airflow/ci:latest"}},
+    )
+
+    # Use the zip binary, which is only found in this special docker image
+    two_task = PythonOperator(
+        task_id="two_task",
+        python_callable=use_zip_binary,
+        executor_config={"KubernetesExecutor": {"image": "airflow/ci_zip:latest"}},
+    )
+
+    # Limit resources on this operator/task with node affinity & tolerations
+    three_task = PythonOperator(
+        task_id="three_task",
+        python_callable=print_stuff,
+        executor_config={
+            "KubernetesExecutor": {
+                "request_memory": "128Mi",
+                "limit_memory": "128Mi",
+                "tolerations": tolerations,
+                "affinity": affinity,
+            }
+        },
+    )
+
+    # Add arbitrary labels to worker pods
+    four_task = PythonOperator(
+        task_id="four_task",
+        python_callable=print_stuff,
+        executor_config={"KubernetesExecutor": {"labels": {"foo": "bar"}}},
+    )
+
+    start_task >> [one_task, two_task, three_task, four_task]
